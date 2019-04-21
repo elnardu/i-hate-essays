bc1ce9da36c03172f520c94f6ce82289998ee4da
Luke Gregurich <lgreguri@purdue.edu>
2019-04-21 16:40:32 -0400

    begin app implementation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,59 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <div id="nav">
+            <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>
+                Logout
+            </router-link>
+        </div>
+        <router-view @authenticated="setAuthenticated" />
+    </div>
 </template>
 
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                authenticated: false,
+                hardCodeAccount: {
+                    username: "lgreguri",
+                    password: "password"
+                }
+            }
+        },
+        mounted() {
+            if(!this.authenticated) {
+                this.$router.replace({ name: "login" });
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+            }
+        }
+    }
+</script>
+
 <style>
-#app, body, html {
+/* #app, body, html {
   width: 100vw;
   height: 100vh;
   padding: 0;
   margin: 0;
+} */
+body {
+        background-color: rgb(184, 184, 184);
+}
+h1 {
+    padding: 0;
+    margin-top: 0;
+}
+#app {
+    width: 1024px;
+    margin: auto;
 }
 </style>
+          
