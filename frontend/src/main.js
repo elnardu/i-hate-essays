7ff251c1ee7c7dcb62ba34216d8e4f7876f8123d
Luke Gregurich <lgreguri@purdue.edu>
2019-04-22 15:43:31 -0400

    update login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,18 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
