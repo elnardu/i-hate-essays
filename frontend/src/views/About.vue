2125678c0a43f27b528b452223300d89b22b0193
Luke Gregurich <lgreguri@purdue.edu>
2019-04-22 15:49:25 -0400

    update about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This tool is supposed to help people like me, 
+      who often get stuck while writing an essay by providing smart autocomplete. 
+      It is also supposed to help writing long and unnecessary sentences 
+      in order to increase the word count.</h1>
   </div>
 </template>
