b4b258cb3d3ba226cb6a1fe75063ad57b7f7768b
Luke Gregurich <lgreguri@purdue.edu>
2019-04-23 17:15:01 -0400

    more formatting, homepage

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,19 +1,18 @@
 <template>
-  <div>
+  <div class="topnav">    
     <nav class="navbar navbar-light bg-light">
-    
-    <img src="../assets/hateEssays.png" width="80" height="80">
-      
-    <a class="navbar-brand" href="#home">
-      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-      Home
-    </a>
-    <a class="navbar-brand" href="#docs">
-      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-      Documents
-    </a>
-    <button class="btn btn-primary btn-lg float-right" onClick= {Logout}>Logout</button>
-</nav>
+      <img src="../assets/hateEssays.png" width="80" height="80">
+        
+      <a class="navbar-brand" href="#home">
+        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30"  alt="">
+        Home
+      </a>
+      <a class="navbar-brand" href="#docs">
+        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        My Documents
+      </a>
+      <button class="btn btn-primary btn-lg float-right" onClick= {Logout}>Logout</button>
+    </nav>
   </div>
 </template>
 
@@ -26,4 +25,31 @@ export default {
 </script>
 
 <style scoped>
+.topnav a {
+  float: left;
+  color: #000000;
+  text-align: center;
+  padding: 16px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+}
+/* Change the color of links on hover */
+.topnav a:hover {
+background-color: #ddd;
+color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+/* background-color: rgb(241, 134, 46); */
+color: white;
+}
+
+button:hover {
+    opacity: 0.75;
+}
+.navbar {
+
+}
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <p>This tool is supposed to help people like me, who often get stuck while writing an essay by providing smart autocomplete. It is also supposed to help writing long and unnecessary sentences in order to increase the word count.</p>
-  </div>
+    <img alt="Vue logo" src="../assets/writers-block-eraser.jpg">
+    <figure class="figure">
+  <!-- <img src="../assets/writers-block-eraser.jpg" class="figure-img img-fluid rounded" alt="writersblock" > -->
+  <figcaption class="figure-caption text-right">This tool is supposed to help people like me, who often get stuck while writing an essay by providing smart autocomplete. It is also supposed to help writing long and unnecessary sentences in order to increase the word count.</figcaption>
+</figure>
+</div>
   <!-- <blockquote class="blockquote">
   <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
   </blockquote> -->
