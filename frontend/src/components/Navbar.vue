2ada92ee7f97ce604fdf7af0b36f14561d632acf
Luke Gregurich <lgreguri@purdue.edu>
2019-04-23 16:42:53 -0400

    add website logo

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <nav class="navbar navbar-light bg-light">
+    
+    <img src="../assets/hateEssays.png" width="80" height="80">
+      
     <a class="navbar-brand" href="#home">
       <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
       Home
@@ -9,17 +12,16 @@
       <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
       Documents
     </a>
-    <a class="navbar-brand" href="#login">
-      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" align="right" height="30" class="d-inline-block align-top" alt="">
-      Logout
-    </a>
+    <button class="btn btn-primary btn-lg float-right" onClick= {Logout}>Logout</button>
 </nav>
   </div>
 </template>
 
 <script>
 export default {
-    
+  Logout(){
+
+  }
 };
 </script>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,6 +3,9 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <p>This tool is supposed to help people like me, who often get stuck while writing an essay by providing smart autocomplete. It is also supposed to help writing long and unnecessary sentences in order to increase the word count.</p>
   </div>
+  <!-- <blockquote class="blockquote">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  </blockquote> -->
 </template>
 
 <script>
