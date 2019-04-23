c1303c13a11bb80b4d226a7ab400d027e78ea862
Luke Gregurich <lgreguri@purdue.edu>
2019-04-23 16:20:10 -0400

    begin navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#home">
+      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      Home
+    </a>
+    <a class="navbar-brand" href="#docs">
+      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      Documents
+    </a>
+    <a class="navbar-brand" href="#login">
+      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" align="right" height="30" class="d-inline-block align-top" alt="">
+      Logout
+    </a>
+</nav>
+  </div>
 </template>
 
 <script>
